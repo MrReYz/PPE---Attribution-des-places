51ba90ec09a0295022bb3cc3cb9a8f92378d5be1
MrReYz <jerome.chaure@orange.fr>
2016-09-30 10:37:17 +0200

    Base de donnée à jour

--- parking.sql
+++ parking.sql
@@ -8,15 +8,13 @@
 #------------------------------------------------------------
 
 CREATE TABLE utilisateur(
-        Id_utilisateur Int NOT NULL ,
-        nom            Varchar (25) ,
-        prenom         Varchar (25) ,
-        email          Varchar (25) ,
-        adresse        Varchar (25) ,
-        login          Varchar (25) ,
-        mot_de_passe   Varchar (25) ,
-        fil_d_attente  Int ,
-        PRIMARY KEY (Id_utilisateur )
+        id_utilisateur int (11) Auto_increment  NOT NULL ,
+        nom            Varchar (25) NOT NULL ,
+        prenom         Varchar (25) NOT NULL ,
+        email          Varchar (25) NOT NULL ,
+        password       Varchar (25) NOT NULL ,
+        fil_d_attente  Int NOT NULL ,
+        PRIMARY KEY (id_utilisateur )
 )ENGINE=InnoDB;
 
 
@@ -47,11 +45,11 @@ CREATE TABLE date(
 CREATE TABLE attribut(
         date_fin       Date ,
         id_place       Int NOT NULL ,
-        Id_utilisateur Int NOT NULL ,
+        id_utilisateur Int NOT NULL ,
         date_debut     Date NOT NULL ,
-        PRIMARY KEY (id_place ,Id_utilisateur ,date_debut )
+        PRIMARY KEY (id_place ,id_utilisateur ,date_debut )
 )ENGINE=InnoDB;
 
 ALTER TABLE attribut ADD CONSTRAINT FK_attribut_id_place FOREIGN KEY (id_place) REFERENCES places(id_place);
-ALTER TABLE attribut ADD CONSTRAINT FK_attribut_Id_utilisateur FOREIGN KEY (Id_utilisateur) REFERENCES utilisateur(Id_utilisateur);
+ALTER TABLE attribut ADD CONSTRAINT FK_attribut_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur);
 ALTER TABLE attribut ADD CONSTRAINT FK_attribut_date_debut FOREIGN KEY (date_debut) REFERENCES date(date_debut);
